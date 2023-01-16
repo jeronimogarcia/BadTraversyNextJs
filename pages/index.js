5c6b38a26297edc9e2da8d77a3a591df08b8a924
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-16 11:37:46 +0100

    Displaying Events in HomePage and EventsPage

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,33 @@
+import Link from "next/link";
 import Layout from "@/components/layouts/Layout";
+import EventItem from "@/components/EventItem";
 import { API_URL } from "@/config/index";
 
-export default function HomePage({events}) {
+export default function HomePage({ events }) {
   return (
     <Layout>
       <h1>HomePage</h1>
-      
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href="/events" className="btn-secondary">
+          View All Events
+        </Link>
+      )}
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/api/events`)
-  const events = await res.json()
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
 
-  return{
-    props: {events},
-    revalidate: 10
-  }
+  return {
+    props: { events: events.slice(0, 3) },
+    revalidate: 10,
+  };
 }
