24abe63fb069696a614a52e788d952a4cc173a8a
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-17 11:20:38 +0100

    Add Event Page.

--- pages/events/add.js
+++ pages/events/add.js
@@ -1,11 +1,116 @@
-import Layout from "@/components/layouts/Layout"
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { API_URL } from "@/config/index";
 
-const AddPage = () => {
+import Layout from "@/components/layouts/Layout";
+import styles from "@/styles/AddEvent.module.css";
+
+const AddEventPage = () => {
+  const [values, setValues] = useState({
+    name: "",
+    performers: "",
+    venue: "",
+    address: "",
+    date: "",
+    time: "",
+    description: "",
+  });
+
+  const router = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventSubmit();
+  };
+
+  const handleInputChange = () => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
   return (
-    <Layout title='Add new event'>
-        <h1>Add Event</h1>
-    </Layout>
-  )
-}
+    <Layout title="Add new event">
+      <Link href="/events">Go Back</Link>
+      <h1>Add Event</h1>
 
-export default AddPage
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.grid}>
+          <div>
+            <label htmlFor="name">Event Name</label>
+            <input
+              type="text"
+              id="id"
+              name="name"
+              value={values.name}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="performers">Event Performers</label>
+            <input
+              type="text"
+              id="performers"
+              name="performers"
+              value={values.performers}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="venue">Venue</label>
+            <input
+              type="text"
+              id="venue"
+              name="venue"
+              value={values.venue}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="address">Address</label>
+            <input
+              type="text"
+              id="address"
+              name="address"
+              value={values.address}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="date">Date</label>
+            <input
+              type="date"
+              id="date"
+              name="date"
+              value={values.date}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="time">Time</label>
+            <input
+              type="text"
+              id="time"
+              name="time"
+              value={values.time}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <div>
+          <label htmlFor="description">Event Description</label>
+          <textarea
+            type="text"
+            id="description"
+            name="description"
+            value={values.description}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <input className="btn" type="submit" value="Add Event" />
+      </form>
+    </Layout>
+  );
+};
+
+export default AddEventPage;
