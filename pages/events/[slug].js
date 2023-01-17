ba796792280e8ae7cbd9c5e16f39a2079c7f3785
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-17 10:11:01 +0100

    Connecting Strapi. Configs, image domain, date formate, url fetch, querys.

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -24,14 +24,14 @@ const EventPage = ({ evt }) => {
         </div>
 
         <span>
-          {evt.date} at {evt.time}
+          {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
         </span>
 
         <h1>{evt.name}</h1>
 
         {evt.image && (
           <div className={styles.image}>
-            <Image src={evt.image} width={960} height={600} />
+            <Image src={evt.image.formats.medium.url} width={960} height={600} />
           </div>
         )}
 
@@ -53,7 +53,7 @@ const EventPage = ({ evt }) => {
 export default EventPage;
 
 export async function getStaticPaths() {
-  const res = await fetch(`${API_URL}/api/events`);
+  const res = await fetch(`${API_URL}/events`);
 
   const events = await res.json();
 
@@ -66,7 +66,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const res = await fetch(`${API_URL}/events?slug=${slug}`);
 
   const events = await res.json();
 
