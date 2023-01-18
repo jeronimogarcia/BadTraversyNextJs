e2b4fc5074eec770fa3b73d59c82da53a5f67045
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-18 15:41:04 +0100

    Logout and destroy cookie

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -44,7 +44,15 @@ export const AuthProvider = ({ children }) => {
   };
 
   // Logout User
-  const logout = async () => {};
+  const logout = async () => {
+    const res = await fetch(`${NEXT_URL}/api/logout`, {
+      method: 'POST'
+    })
+    if(res.ok){
+      setUser(null)
+      router.push('/')
+    }
+  };
 
   // Check if user is logged in
   const checkUserLoggedIn = async (user) => {
